fedf101df39d6f92d08a1bac81db0386a60bc74d
RonAs <96978170+ronasolin12@users.noreply.github.com>
2022-03-14 13:07:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 
 const actionRoute = require('./action');
 app.use('/api', actionRoute);
+
 const port= 5000;
 app.listen(port, function(){
     console.log(`server is working via port ${port}`);
