fdf1c2dd865d55b1734a43d40d556f47a8cbce05
RonAs <96978170+ronasolin12@users.noreply.github.com>
2022-03-14 12:02:06 +0200

    14032022

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-const actionRoute = require('./actions');
-use.app('/api', actionRoute);
+const actionRoute = require('./action');
+app.use('/api', actionRoute);
 const port= 5000;
 app.listen(port, function(){
     console.log(`server is working via port ${port}`);
