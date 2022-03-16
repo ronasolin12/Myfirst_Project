6bdf3fa0c722c8a1d63032e67dc39f15f3631f11
ronasolin12 <ronasolin12@gmail.com>
2022-03-16 10:38:28 +0200

    Update action.js

--- action.js
+++ action.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+
+let users = [];
+
+router.post('/register',(req,res)=>{
+
+    //const user_email = req.body.email;
+    //const user_password = req.body.password;
+
+    const {email,password} =req.body;
+
+    users.push({email:email,password:password});
+
+    return res.status(200).json({
+        message: users
+    });
+
+})
+
+
+
 router.get('/greeting',(req,res) => {
     return res.status(200).json({
         message:'welcome to my node.js API'
     });
-
 });
+
+
 module.exports = router;
